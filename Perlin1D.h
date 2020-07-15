6e1318560c7f6947f25ffaac6240d5af6e303d00
Mithul Manivannan <36342151+MithulM@users.noreply.github.com>
2020-07-15 16:12:08 -0500

    Added comments
    Added Comments

--- Perlin1D.h
+++ Perlin1D.h
@@ -1,3 +1,11 @@
+/*
+What is does:
+	Show a hight map of 1D perlin noise
+
+Why:
+	To show the smooth randomness of perlin noise.
+*/
+
 #pragma once
 #include "olcPixelGameEngine.h"
 #include <random>
@@ -6,9 +14,10 @@
 class Perlin1D : public olc::PixelGameEngine
 {
 private:
+	// Seed array
 	float seed[WIDTH] = { 0 };
 	
-	
+	// initialize params for noise
 	int octaves = 0;
 	float bias = 2.0f;
 
@@ -20,6 +29,7 @@ public:
 
 	bool OnUserCreate() override
 	{
+		// Create seed for perlin noise
 		for (int i = 0; i < WIDTH; i++) seed[i] = (float)rand() / (float)RAND_MAX;
 		
 		return true;
@@ -28,6 +38,8 @@ public:
 	bool OnUserUpdate(float fElapsedTime) override
 	{
 		Clear(olc::BLACK);
+
+		// Generate perlin noise
 		float noise[WIDTH] = { 0 };
 
 		float scale = 1.0f;
@@ -43,26 +55,38 @@ public:
 			}
 		}
 
+		// map the noise from 0 to half of ScreenHeight 
 		for (int i = 0; i < WIDTH; i++)
 			noise[i] *= ScreenHeight() / 2 / total;
 
+		///////////////////////
+		// Actions
+		///////////////////////
+
+		// Press space to create a new seed for noise
 		if (GetKey(olc::SPACE).bPressed)
 			for (int i = 0; i < WIDTH; i++) seed[i] = (float)rand() / (float)RAND_MAX;
-
+		
+		// Increase the detail of noise by pressing W
 		if (GetKey(olc::W).bPressed)
-			octaves++;a
+			octaves++;
 
+		// Incrase the bias by hitting D
 		if (GetKey(olc::D).bPressed)
 			bias += 0.2f;
 		
+		// Decrease the bias by hittting A
 		if (GetKey(olc::A).bPressed)
 			bias -= 0.2f;
 
+		// Set bias to .2 if it goes below .2
 		bias = max(0.2f, bias);
 
+		// Set max octives log2(WIDTH)
 		if (octaves > (int)(log(WIDTH) / log(2)))
 			octaves = 0;
 
+		// Draw the noise as a hight map
 		for (int i = 1; i < min(WIDTH, ScreenWidth()); i++)
 			DrawLine(olc::vi2d(i, ScreenHeight() / 2 - noise[i]), olc::vi2d(i - 1, ScreenHeight() / 2 - noise[i - 1]), olc::DARK_RED);
 
